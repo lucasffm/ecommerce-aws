0b071947f8a1325c56b55673b6c8993bb7cddff6
Lucas Mendes <lucasffm@gmail.com>
2023-04-20 21:57:28 -0300

    feat: add products fetch lambda

---
+++ lambda/products/productsFetchFunction.ts
@@ -0,0 +1,35 @@
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  Context,
+} from "aws-lambda";
+
+export async function handler(
+  event: APIGatewayProxyEvent,
+  context: Context
+): Promise<APIGatewayProxyResult> {
+  const method = event.httpMethod;
+  const lambdaRequestId = context.awsRequestId;
+  const apiRequestId = event.requestContext.requestId;
+  console.log(
+    `API Gateway Request ID: ${apiRequestId} - Lambda Request Id: ${lambdaRequestId}`
+  );
+
+  if (event.resource === "/products") {
+    if (method === "GET") {
+      console.log("GET");
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: "GET Products - OK",
+          lambdaRequestId,
+          apiRequestId,
+        }),
+      };
+    }
+  }
+  return {
+    statusCode: 400,
+    body: JSON.stringify({ message: "Bad Request" }),
+  };
+}
---
+++ lib/products-app-stack.ts
@@ -0,0 +1,26 @@
+import { Duration, Stack, StackProps } from "aws-cdk-lib";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Construct } from "constructs";
+
+export class ProductsAppStack extends Stack {
+  readonly productsFetchHandler: NodejsFunction;
+
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+    this.productsFetchHandler = new NodejsFunction(
+      this,
+      "ProductsFetchFunction",
+      {
+        functionName: "ProductsFetchFunction",
+        entry: "lambda/products/productsFetchFunction.ts",
+        handler: "handler",
+        memorySize: 128,
+        timeout: Duration.seconds(5),
+        bundling: {
+          minify: true,
+          sourceMap: false
+        }
+      }
+    );
+  }
+}
