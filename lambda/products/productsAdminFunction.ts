f7ca38c2f4a1d545e6b74e43f0080e4bddba8465
Lucas Mendes <lucasffm@gmail.com>
2023-04-25 11:45:50 -0300

    feat: use product repository

--- lambda/products/productsAdminFunction.ts
+++ lambda/products/productsAdminFunction.ts
@@ -3,6 +3,12 @@ import {
   APIGatewayProxyResult,
   Context,
 } from "aws-lambda";
+import { DocumentClient } from "aws-sdk/clients/dynamodb";
+import { Product, ProductRepository } from "/opt/nodejs/productsLayer";
+
+const productsDdb = process.env.PRODUCTS_DDB!;
+const ddbClient = new DocumentClient();
+const productRepository = new ProductRepository(ddbClient, productsDdb);
 
 export async function handler(
   event: APIGatewayProxyEvent,
@@ -14,9 +20,12 @@ export async function handler(
 
   if (event.resource === "/products") {
     console.log("POST /products");
+    const product = JSON.parse(event.body!) as Product;
+    const createdProduct = await productRepository.createProduct(product);
+
     return {
       statusCode: 201,
-      body: "POST /products",
+      body: JSON.stringify(createdProduct),
     };
   }
 
@@ -24,17 +33,40 @@ export async function handler(
     const productId = event.pathParameters!.id as string;
     if (event.httpMethod === "PUT") {
       console.log(`PUT /products/${productId}`);
-      return {
-        statusCode: 200,
-        body: `PUT /products/${productId}`,
-      };
+      const product = JSON.parse(event.body!) as Product;
+      try {
+        const productUpdated = await productRepository.updateProduct(
+          productId,
+          product
+        );
+
+        return {
+          statusCode: 200,
+          body: JSON.stringify(productUpdated),
+        };
+      } catch (error) {
+        return {
+          statusCode: 404,
+          body: "Product Not found",
+        };
+      }
     }
     if (event.httpMethod === "DELETE") {
       console.log(`DELETE /products/${productId}`);
-      return {
-        statusCode: 200,
-        body: `DELETE /products/${productId}`,
-      };
+      try {
+        const product = await productRepository.deleteProduct(productId);
+
+        return {
+          statusCode: 200,
+          body: JSON.stringify(product),
+        };
+      } catch (error) {
+        console.error((<Error>error).message);
+        return {
+          statusCode: 404,
+          body: (<Error>error).message,
+        };
+      }
     }
   }
 
