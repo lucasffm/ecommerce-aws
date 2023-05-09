32af265e1a7500e46529871404718756ecc6ad58
Lucas Mendes <lucasffm@gmail.com>
2023-05-09 00:47:46 -0300

    feat: add products request validator

--- lib/ecommerce-api-stack.ts
+++ lib/ecommerce-api-stack.ts
@@ -1,8 +1,10 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import {
   AccessLogFormat,
+  JsonSchemaType,
   LambdaIntegration,
   LogGroupLogDestination,
+  Model,
   RequestValidator,
   RestApi,
 } from "aws-cdk-lib/aws-apigateway";
@@ -72,7 +74,55 @@ export class EcommerceApiStack extends Stack {
       requestValidator: orderDeleteValidation,
     });
     // POST /orders
-    ordersResource.addMethod("POST", ordersIntegration);
+    const orderRequestValidator = new RequestValidator(
+      this,
+      "OrderRequestValidator",
+      {
+        restApi: api,
+        requestValidatorName: "OrderRequestValidator",
+        validateRequestBody: true,
+      }
+    );
+
+    const orderRequestModel = new Model(this, "OrderRequestModel", {
+      restApi: api,
+      modelName: "OrderRequestModel",
+      schema: {
+        type: JsonSchemaType.OBJECT,
+        properties: {
+          email: { type: JsonSchemaType.STRING },
+          productIds: {
+            type: JsonSchemaType.ARRAY,
+            minItems: 1,
+            items: { type: JsonSchemaType.STRING },
+          },
+          payment: {
+            type: JsonSchemaType.STRING,
+            enum: ["CASH", "DEBIT_CARD", "CREDIT_CARD"],
+          },
+          shipping: {
+            type: JsonSchemaType.OBJECT,
+            properties: {
+              type: {
+                type: JsonSchemaType.STRING,
+                enum: ["ECONOMIC", "URGENT"],
+              },
+              carrier: {
+                type: JsonSchemaType.STRING,
+                enum: ["CORREIOS", "FEDEX"],
+              },
+            },
+          },
+        },
+        required: ["email", "productIds", "payment", "shipping"],
+      },
+    });
+    ordersResource.addMethod("POST", ordersIntegration, {
+      requestValidator: orderRequestValidator,
+      requestModels: {
+        "application/json": orderRequestModel,
+      },
+    });
   }
 
   private createProductsService(props: EcommerceApiStackProps, api: RestApi) {
@@ -91,9 +141,44 @@ export class EcommerceApiStack extends Stack {
     const productIdResource = productsResource.addResource("{id}");
     productIdResource.addMethod("GET", productsFetchIntegration);
     // POST /products
-    productsResource.addMethod("POST", productsAdminIntegration);
+    const ProductRequestValidator = new RequestValidator(
+      this,
+      "ProductRequestValidator",
+      {
+        restApi: api,
+        requestValidatorName: "ProductRequestValidator",
+        validateRequestBody: true,
+      }
+    );
+    const productRequestModel = new Model(this, "ProductRequestModel", {
+      restApi: api,
+      modelName: "ProductRequestModel",
+      schema: {
+        type: JsonSchemaType.OBJECT,
+        properties: {
+          productName: { type: JsonSchemaType.STRING },
+          price: { type: JsonSchemaType.NUMBER },
+          code: { type: JsonSchemaType.STRING },
+          model: { type: JsonSchemaType.STRING },
+          productUrl: { type: JsonSchemaType.STRING },
+        },
+        required: ["productName", "price", "code"],
+      },
+    });
+    productsResource.addMethod("POST", productsAdminIntegration),
+      {
+        requestValidator: ProductRequestValidator,
+        requestModels: {
+          "application/json": productRequestModel,
+        },
+      };
     // PUT /products/{id}
-    productIdResource.addMethod("PUT", productsAdminIntegration);
+    productIdResource.addMethod("PUT", productsAdminIntegration, {
+      requestValidator: ProductRequestValidator,
+      requestModels: {
+        "application/json": productRequestModel,
+      },
+    });
     // DELETE /products/{id}
     productIdResource.addMethod("DELETE", productsAdminIntegration);
   }
